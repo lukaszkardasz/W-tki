8b6492bbf70b5e529672d51480a134425b799ec0
n2god <n2god@o2.pl>
2018-12-02 11:00:32 +0100

    callable with multiple threads

--- src/main/java/Watki/CallableExecutor.java
+++ src/main/java/Watki/CallableExecutor.java
@@ -1,13 +1,15 @@
 package Watki;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.*;
 
 public class CallableExecutor  implements Callable<String> {
 
     @Override
     public String call() throws Exception {
-        System.out.println("Hello thread!");
-        return "test";
+        System.out.println("Hello thread: ");
+        return Thread.currentThread().getName();
     }
 
 
@@ -15,12 +17,30 @@ public class CallableExecutor  implements Callable<String> {
 
         System.out.println("Od tego sie zaczyna!");
 
-        //Wywoluje nowy watek z góry w mainie + tworzymy eecutora, ktory wywoluje obiekty callable
+        //runSingleThread();
+        runMultipleThreads();
+
+    }
+
+    private static void runMultipleThreads() throws InterruptedException {
+        ExecutorService executeMultipleService = Executors.newFixedThreadPool(8);
+        //definiujemy liste zadan wykonania przez watki
+        List<Callable<String>> tasks = new ArrayList<>();
+        //dodaje zadania petla for - on tworzy 10 obiektów callable wykonywane przez 8 watkow
+        for (int i = 0 ; i < 10; i++){
+            tasks.add(new CallableExecutor());
+        }
+        executeMultipleService.invokeAll(tasks);
+        Thread.sleep(1000);
+
+    }
+
+    private static void runSingleThread() throws InterruptedException, ExecutionException {
+        //Wywoluje nowy watek z góry w mainie + tworzymy eecutora, ktory wywoluje obiekty callable - Executory sa rozne, odpalajace jeden watek lub wiecej
         ExecutorService executorService = Executors.newSingleThreadExecutor();
 
-        //metoda submit uruchamia metode call, ktora odpala watek z call
+        //Executor metoda submit uruchamia metode call, ktora odpala watek z call
         Future<String> threadResult = executorService.submit(new CallableExecutor());
-
 
         System.out.println("Wynik: " + threadResult.get());
         executorService.shutdown();
